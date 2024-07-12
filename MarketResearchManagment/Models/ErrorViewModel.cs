fb6af8bde55518d13dbdbed8d801ae89f932d710
kamro <kamro@KAMRON>
2024-07-12 06:57:39 -0400

    Add project files.

---
+++ MarketResearchManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MarketResearchManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
