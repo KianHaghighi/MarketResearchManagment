7f87e565471f067d3f7e1ec2da22e2ff5a12885e
Kian Haghighi <kianhaghighics@gmail.com>
2024-07-14 12:55:14 -0400

    Fixes Market Map image not being displayed

--- MarketResearchManagment/Controllers/MarketMapController.cs
+++ MarketResearchManagment/Controllers/MarketMapController.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarketResearchManagment.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MarketResearchManagment.Controllers
 {
     public class MarketMapController : Controller
     {
-        public IActionResult Index()
+        /*public IActionResult Index()
         {
             return View();
+        }*/
+        public ActionResult Index()
+        {
+            // Optionally, fetch market map data from a database or service
+            var marketMaps = new List<MarketMap>
+        {
+            new MarketMap { Id = 1, Name = "PropTech Map", Description = "This market map displays and segments the companies in the Real Estate Tech Space", ImageUrl= "/Content/MarketMaps/PropTechMap.png"},
+        };
+
+            return View(marketMaps); 
         }
-        
+
         [HttpPost]
         public ActionResult AnalyzeMarket(string marketName)
         {
