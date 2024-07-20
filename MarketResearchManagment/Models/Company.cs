a51073863776bdf0ac4d4a69b713f38d6d48e6c3
Kian Haghighi <kianHaghighics@gmail.com>
2024-07-20 14:20:29 -0400

    Add product hunt data and field 'Topic' to Company model

--- MarketResearchManagment/Models/Company.cs
+++ MarketResearchManagment/Models/Company.cs
@@ -1,9 +1,12 @@
-﻿namespace MarketResearchManagment.Models
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace MarketResearchManagment.Models
 {
     public class Company
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public decimal Price { get; set; }
+        public string? Topic { get; set; }
     }
 }
